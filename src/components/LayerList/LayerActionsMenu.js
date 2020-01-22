0af8918a362b6b76f8832ee5532aabd5738160cf
brambow <bates@cartolab.com>
2020-01-22 15:52:08 -0500

    test: basic test for new actionMenu

--- src/components/LayerList/LayerActionsMenu.js
+++ src/components/LayerList/LayerActionsMenu.js
@@ -11,35 +11,38 @@ import {
 import '@reach/menu-button/styles.css';
 
 const LayerActionsMenu = ({ layerActions }) => {
-  return (
-    <Menu className="cl-layer-list-action-menu">
-      <MenuButton
-        sx={{
-          color: 'text',
-          bg: 'background',
-          borderStyle: 'none',
-          fontSize: 1
-        }}
-      >
-        ...
-      </MenuButton>
-      <MenuPopover sx={{ zIndex: 3, fontFamily: 'body' }}>
-        <MenuItems>
-          {layerActions.map(action => {
-            return (
-              <MenuItem
-                onSelect={() => {
-                  action.action();
-                }}
-              >
-                {action.title}
-              </MenuItem>
-            );
-          })}
-        </MenuItems>
-      </MenuPopover>
-    </Menu>
-  );
+  if (layerActions && layerActions.length > 0) {
+    return (
+      <Menu className="cl-layer-list-action-menu">
+        <MenuButton
+          sx={{
+            color: 'text',
+            bg: 'background',
+            borderStyle: 'none',
+            fontSize: 1
+          }}
+        >
+          ...
+        </MenuButton>
+        <MenuPopover sx={{ zIndex: 3, fontFamily: 'body' }}>
+          <MenuItems>
+            {layerActions.map(action => {
+              return (
+                <MenuItem
+                  key={action.title}
+                  onSelect={() => {
+                    action.action();
+                  }}
+                >
+                  {action.title}
+                </MenuItem>
+              );
+            })}
+          </MenuItems>
+        </MenuPopover>
+      </Menu>
+    );
+  } else return null;
 };
 
 export default LayerActionsMenu;
