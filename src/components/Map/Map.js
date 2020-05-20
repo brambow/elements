52904d66c7a8e34ac8313b168c78865a666e374f
brambow <bates@cartolab.com>
2020-05-20 09:23:07 -0400

    feat: add GL support alert

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,8 +2,9 @@ import React, { useEffect, useContext } from 'react';
 import { Box } from 'theme-ui';
 import mapboxgl from 'mapbox-gl';
 import DefaultContext from '../../DefaultContext';
+import AlertModal from '../_common/AlertModal';
 
-const Map = ({ mapOptions, mapLayers, sx, ...rest }) => {
+const Map = ({ mapOptions, mapLayers, supportMessage, sx, ...rest }) => {
   let map;
   const config = useContext(DefaultContext);
   const { setMap } = config;
@@ -24,6 +25,19 @@ const Map = ({ mapOptions, mapLayers, sx, ...rest }) => {
 
     addLayers(mapLayers);
   }, []);
+
+  if (!mapboxgl.supported()) {
+    return (
+      <AlertModal
+        message={
+          supportMessage ||
+          `WARNING! Your browser does not currently support this application, which requires MapboxGL. 
+    Please ensure you are using the latest version of Chrome, Firefox, or Edge.
+    Additionally, check https://get.webgl.org/ for compatibility with your system.`
+        }
+      />
+    );
+  }
 
   return (
     <Box
