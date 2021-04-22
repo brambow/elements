186edcfb0b8703235c2d8e16512414c612711d06
brambow <bates@cartolab.com>
2021-04-22 09:31:39 -0400

    chore: update rollup babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import replace from 'rollup-plugin-replace';
 import postcss from 'rollup-plugin-postcss';
 import svg from 'rollup-plugin-svg';
