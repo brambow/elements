d3e6d4aa14e9b0e4effd487efd50f7753d69d2c4
brambow <bates@cartolab.com>
2022-02-10 17:04:14 -0500

    feat: add context value for drawMode

--- src/components/_common/ElementsProvider.js
+++ src/components/_common/ElementsProvider.js
@@ -1,33 +1,19 @@
 import React, { useState } from 'react';
-// import { ThemeProvider as EmotionProvider } from 'emotion-theming';
 import { ThemeProvider as ThemeUIProvider } from 'theme-ui';
 import DefaultContext from '../../DefaultContext';
 
-const ElementsProvider = ({
-  children,
-  context,
-  theme,
-  // themeUI,
-  mapOverride
-}) => {
+const ElementsProvider = ({ children, context, theme, mapOverride }) => {
   const ctx = context || DefaultContext;
   const ContextProvider = ctx.Provider || ctx;
   const [map, setMap] = useState(null);
+  const [drawMode, setDrawMode] = useState(null);
 
   const value = {
     map: mapOverride || map,
-    setMap
+    setMap,
+    drawMode,
+    setDrawMode
   };
-
-  // let provider = (
-  //   <EmotionProvider theme={theme || {}}>{children}</EmotionProvider>
-  // );
-
-  // if (themeUI === true) {
-  //   provider = (
-  //     <ThemeUIProvider theme={theme || {}}>{children}</ThemeUIProvider>
-  //   );
-  // }
 
   const provider = (
     <ThemeUIProvider theme={theme || {}}>{children}</ThemeUIProvider>
